7b5873164f43df18d96de9782ea98581f1648b53
Ricardo Gallegos González <gallegos.rickgalle@gmail.com>
2020-07-22 22:59:55 -0500

    css menu style e index

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="menu.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/menu.css">
     <title>AR Agencia</title>
 </head>
 <body>
