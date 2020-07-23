b793698871abaff0b70c8eb8fc015d0d642cb5c2
Ricardo Gallegos González <gallegos.rickgalle@gmail.com>
2020-07-22 21:39:40 -0500

    imagenes/css_menu/menu/index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="menu.css">
+    <title>AR Agencia</title>
+</head>
+<body>
+    <header>
+        <?php
+        require ("menu.html");
+        ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="menu.js"></script>
+</body>
+</html>
