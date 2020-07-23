b51d6a2ebf1946a100bec89f00f8745b116f4008
Alejandro <alejandrochap09@gmail.com>
2020-07-23 00:46:33 -0500

    Fuente, Font Size, Font weight

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/menu.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet"> 
     <title>AR Agencia</title>
 </head>
 <body>
