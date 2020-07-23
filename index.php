4b3198887348507d4d2d99268998a37c54cfcce3
Alejandro <alejandrochap09@gmail.com>
2020-07-23 09:16:51 -0500

    Chingos de cosas (Logo, barra del logo, tamano del menu de hamburguesa, etc)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/menu.css">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap" rel="stylesheet"> 
@@ -10,9 +11,14 @@
 </head>
 <body>
     <header>
-        <?php
-        require ("menu.html");
-        ?>
+        <div class="container barra-head">
+            <div class="container logo">
+                <img src="img/Logo.jpg" alt="Logo">
+            </div>
+            <?php
+            require ("menu.html");
+            ?>
+        </div>
     </header>
     <main>
 
