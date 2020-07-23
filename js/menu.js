a16a041b9f07943a04ed353c83123ff587b31c8f
Alejandro <alejandrochap09@gmail.com>
2020-07-23 00:09:29 -0500

    javascript menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,5 @@
 const menuIcon = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('.navigation-menu');
-const bgmenu = document.querySelector('')
 menuIcon.addEventListener('click', () =>{
     navbar.classList.toggle('change')
 });
